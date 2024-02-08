671b20367920a209b29044e0c257e6f24f566ce4
Mitchell C. Foote <mcfoote321@gmail.com>
2024-02-08 03:02:36 -0500

    wip

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
+/*
+
+*/
 #include<iostream>
 #include<string>
 #include<stack>
+#include<limits>
+#include<algorithm>
+#include<vector>
+#include<sstream>
 
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 using std::stack;
+using std::getline;
+using std::vector;
 
 int numVar;
 bool a, b, c, d, e;
@@ -14,8 +23,10 @@ bool a, b, c, d, e;
 void evaluator();
 void getVar();
 void getVal();
-void validateVal(bool in);
+void validateVal(bool &in);
 void getExp();
+vector<string> tokenize(const string &expression);
+bool isTokenValid(const string& token);
 
 int main() {
 
@@ -98,21 +109,76 @@ void getVal() {
 
 }
 
-void validateVal(bool in) {
+void validateVal(bool &in) {
+    string input;
 
-    while(true) {
-        cin >> in;
-        if(cin.fail()) {
-            cin.clear();
-            cout << endl << "Invalid input please enter true or false" << endl;
-        } else if(in == true || in == false) {
+    while (true) {
+        cin >> input;
+
+        if (input == "true" || input == "false") {
+
+            if (input == "true") {
+                in = true;
+            } else {
+                in = false;
+            }
             break;
+        } else {
+            cout << endl << "Invalid input. Please enter 'true' or 'false': ";
+            cin.clear();
         }
     }
 
+    // Clear any remaining characters in the input buffer
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
+
 void getExp() {
+
+    string expression;
+    getline(cin, expression);
+
+    vector<string> tokens = tokenize(expression);
+
+    //test
+    for(const string &token : tokens) {
+        cout << token << endl;
+    }
+
+
+}
+
+//helper function to assist expression input by tokenizing string stream. 
+vector<string> tokenize(const string &expression) {
+
+    std::istringstream iss(expression);
+    string token;
+    vector<string> tokens;
+    
+    while(iss >> token){
+        tokens.push_back(token);
+    }
+
+    return tokens;
+
+}
+
+bool isTokenValid(const string& token) {
+    
+    const vector<string> validVars = {"a", "b", "c", "d", "e"};
+
+    for (int i = 0; i < numVar; ++i) {
+        if (token == validVars[i]) {
+            return true;  
+        }
+    }
+
+    if (token == "/\\" || token == "\\/" || token == "~") {
+        return true;
+    }
+
+    return false;
 
 }
    
